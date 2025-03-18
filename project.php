4a1eb6b1f72420edb74bd93575f6f4887833ad14
Shadow105EMM <elijasmmai@gmx.ch>
2025-03-18 10:42:35 +0100

    Add 4 projects placeholder

--- project.php
+++ project.php
@@ -15,9 +15,31 @@
       <a class="menu-link" href="">Future</a>
       <a class="menu-link" href="https://github.com/Shadow105EMM/Coding-Website">GitHub</a> 
     </div>
-    </div>
+
 
     <!--Logo-->
       <img class="Logo-E" src="Images/Auftrag_shadow_with_gradient_border.jpg" alt="Logo-E">
-  </body>
+
+<!--
+===========================================================
+   
+                    GRID PLACHOLDE
+
+============================================================
+-->
+    
+    <div class="grid-projects">
+      <div><img class="phimg" src="Images/placeholder.jpg" alt="Placeholder"></div>
+      <div><img class="phimg" src="Images/placeholder.jpg" alt="Placeholder"></div>
+      <div><img class="phimg" src="Images/placeholder.jpg" alt="Placeholder"></div>
+      <div><img class="phimg" src="Images/placeholder.jpg" alt="Placeholder"></div>
+    </div>  
+
+    <div class="text-projects">
+      <div><strong class="phtext">Placeholder 1</strong></div>
+      <div><strong class="phtext">Placeholder 2</strong></div>
+      <div><strong class="phtext">Placeholder 3</strong></div>
+      <div><strong class="phtext">Placeholder 4</strong></div>
+    </div>
+    </body>
 </html>
