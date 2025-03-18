0219627a26885078154c1ebaad1cab2d59d2c3ad
Shadow105EMM <elijasmmai@gmx.ch>
2025-03-18 08:57:46 +0100

    project footer removed

---
+++ project.php
@@ -0,0 +1,23 @@
+<!DOCTYPE html>
+<html lang="de">
+  <head>
+    <!--Connect to style.css-->
+    <link rel="stylesheet" href="style.css">
+    <meta charset="UTF-8" />
+    <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+    <title>Coding Projects</title>
+  </head>
+  <body>
+
+    <!--Menu Links-->
+    <div class="container-link">
+      <a class="menu-link" href="index.php">Home</a>
+      <a class="menu-link" href="">Future</a>
+      <a class="menu-link" href="https://github.com/Shadow105EMM/Coding-Website">GitHub</a> 
+    </div>
+    </div>
+
+    <!--Logo-->
+      <img class="Logo-E" src="Images/Auftrag_shadow_with_gradient_border.jpg" alt="Logo-E">
+  </body>
+</html>
